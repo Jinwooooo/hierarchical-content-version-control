4e2f4d0a708d9582f5baaf4c51ac01c2778a1099
Jinwooooo <chungjinwoo5d@naver.com>
2026-01-23 13:55:32 +0900

    feat: init spring boot (java)

---
+++ src/main/java/com/example/hierarchical_content_version_control/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hierarchical_content_version_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
