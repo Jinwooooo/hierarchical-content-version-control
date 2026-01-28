7c8912f57e0379d7c580aa19da0384236f740ccd
Jinwooooo <chungjinwoo5d@naver.com>
2026-01-28 15:16:55 +0900

    feat (content): integrate dto

--- src/main/java/com/example/hierarchical_content_version_control/domain/content/dto/ContentDTO.java
+++ src/main/java/com/example/hierarchical_content_version_control/domain/content/dto/ContentDTO.java
@@ -1,5 +1,28 @@
 package com.example.hierarchical_content_version_control.domain.content.dto;
 
+import com.example.hierarchical_content_version_control.domain.content.enums.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class ContentDTO {
-    
+    private ObjectId id;
+    private ObjectId parentId;
+    private ObjectId vehicleId;
+    private ObjectId divisionId;
+    private String version;
+    private Integer modifiedCtr;
+    private ContentStatus status;
+    private ContentType type;
+    private String name;
+    private String code;
+    private boolean isDeleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
